a2da1976773cf41a15ebafccc6c82a7e6487e6c8
3rnurr <bisihane@gmail.com>
2026-04-26 05:27:15 +0500

    Update urls.py

--- config/config/urls.py
+++ config/config/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
 
     path("api/token/", TokenObtainPairView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view()),
-    path('', include('django_prometheus.urls')),
 
     path("api/", include("courses.urls")),
     path("api/", include("users.urls")),
