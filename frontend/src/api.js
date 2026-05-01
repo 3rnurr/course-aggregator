6b1a490d5635484a2aad85721c2749dc3773e8af
3rnurr <bisihane@gmail.com>
2026-05-01 19:36:38 +0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://course-aggregator-2.onrender.com/api/",
+  baseURL: "https://competencebooosterr.pythonanywhere.com/api/",
 });
 
 api.interceptors.request.use((config) => {
